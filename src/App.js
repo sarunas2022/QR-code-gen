75ddf4ea245104080a93e014d05d87e71046a66d
Šarūnas Grabauskas <sarunas.grabauskas@gmail.com>
2023-01-04 11:41:00 +0200

    Input field and styling added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import './App.css';
+import { Container, TextField, Typography } from '@mui/material';
 
 export default function App() {
-    return <div>this is qr code generator</div>;
+    return (
+        <Container>
+            <Container sx={{ pt: 4 }}>
+                <Typography variant='h5' align='center' sx={{ py: 3 }}>
+                    Enter text or URL to create QR code
+                </Typography>
+                <TextField fullWidth id='fullWidth'></TextField>
+            </Container>
+        </Container>
+    );
 }
