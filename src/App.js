ce9ecf55806575d8a96d820f3d5460ecb75c3863
Šarūnas Grabauskas <sarunas.grabauskas@gmail.com>
2023-01-09 10:46:15 +0200

    added event handling

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import { Container, TextField, Typography, Button, Box } from '@mui/material';
 export default function App() {
     const [input, setInput] = useState('');
 
+    const inputSubmit = () => {
+        console.log(input);
+    };
+
     return (
         <Container>
             <Container sx={{ pt: 4 }}>
@@ -12,10 +16,17 @@ export default function App() {
                     Enter text or URL to create QR code
                 </Typography>
                 <Box sx={{ display: 'flex' }}>
-                    <TextField fullWidth id='fullWidth'></TextField>
-                    <Button variant='contained'>generate!</Button>
+                    <TextField
+                        onChange={(event) => setInput(event.target.value)}
+                        fullWidth
+                        id='fullWidth'
+                    ></TextField>
+                    <Button onClick={inputSubmit} variant='contained'>
+                        generate!
+                    </Button>
                 </Box>
             </Container>
+            <Container sx={{ pt: 4 }}>{input}</Container>
         </Container>
     );
 }
