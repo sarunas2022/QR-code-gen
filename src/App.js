a5da803c47855aced1049ebbbe639af70433024a
Šarūnas Grabauskas <sarunas.grabauskas@gmail.com>
2023-01-09 10:09:44 +0200

    Added a button, roboto font and removed some files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import { Container, TextField, Typography } from '@mui/material';
+import { Container, TextField, Typography, Button, Box } from '@mui/material';
 
 export default function App() {
+    const [input, setInput] = useState('');
+
     return (
         <Container>
             <Container sx={{ pt: 4 }}>
                 <Typography variant='h5' align='center' sx={{ py: 3 }}>
                     Enter text or URL to create QR code
                 </Typography>
-                <TextField fullWidth id='fullWidth'></TextField>
+                <Box sx={{ display: 'flex' }}>
+                    <TextField fullWidth id='fullWidth'></TextField>
+                    <Button variant='contained'>generate!</Button>
+                </Box>
             </Container>
         </Container>
     );
